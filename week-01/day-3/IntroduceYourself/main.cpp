70f517e2269c6388d892971899155c795a6dc75f
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 13:10:07 +0200

    Complete IntroduceYourself exercise.

---
+++ week-01/day-3/IntroduceYourself/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+    std::cout << "Csaba Berki\n";
+    std::cout << "35\n";
+    std::cout << "1.75\n";
+
+    return 0;
+}
