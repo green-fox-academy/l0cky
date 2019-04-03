b6353d25bd566c319ca0711a40613fe1d525f1f7
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 12:45:01 +0200

    Complete hellome exercise.

---
+++ week-01/day-3/hellome/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Modify this program to greet you instead of the World!
+    std::cout << "Hello me" << std::endl;
+
+    return 0;
+}
