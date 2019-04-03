88646a12f059f71ef8c5af8d834871346b116e8a
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 13:40:05 +0200

    Complete DefineBasicInfo exercise.

---
+++ week-01/day-3/DefineBasicInfo/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Define several things as a variable then print their values
+    // Your name as a string
+    // Your age as an integer
+    // Your height in meters as a double
+    // Whether you are married or not as a boolean
+    std::string name = "l0cky";
+    int age = 35;
+    double height = 1.75;
+    bool married = false;
+
+    std::cout << name << std::endl;
+    std::cout << age << std::endl;
+    std::cout << height<< std::endl;
+    std::cout << married << std::endl;
+
+    return 0;
+}
