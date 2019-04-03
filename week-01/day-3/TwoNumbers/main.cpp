8bc821bbcba2abd3e99320910350c335803f1994
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 13:17:33 +0200

    Complete TwoNumbers exercise.

---
+++ week-01/day-3/TwoNumbers/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Create a program that prints a few operations on two numbers: 22 and 13
+
+    // Print the result of 13 added to 22
+    std::cout << 13 + 22 << std::endl;
+
+    // Print the result of 13 substracted from 22
+    std::cout << 13 - 22 << std::endl;
+
+    // Print the result of 22 multiplied by 13
+    std::cout << 22 * 13 << std::endl;
+
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+    std::cout << 22. / 13 << std::endl;
+
+    // Print the integer part of 22 divided by 13
+    std::cout << 22 % 13 << std::endl;
+
+    // Print the reminder of 22 divided by 13
+    std::cout << 22 % 13 << std::endl;
+
+    return 0;
+}
