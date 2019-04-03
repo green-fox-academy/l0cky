d62a531933d8f69224644cd90d8df848ceb6741b
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 15:38:02 +0200

    Complete IWontCheatOnTheExams exercise.

---
+++ week-01/day-3/IWontCheatOnTheExams/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *args[]) {
+
+    // Create a program that writes this line 100 times:
+    // "I won't cheat on the exam!"
+    for (int i = 0; i < 100; i++) {
+        std::cout << "I won't cheat on the exam!" << std::endl;
+    }
+
+    return 0;
+}
