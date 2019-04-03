50dea8dad040b6ae9f2c296b8f2ffc9b48f8c576
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 13:29:59 +0200

    Complete FavoriteNumber exercise.

---
+++ week-01/day-3/FavoriteNumber/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+    // Store your favorite number in a variable (as an integer)
+    // And print it like this: "My favorite number is: 8"
+    int favouriteNumber = 42;
+    std::cout << "My favourite number is: " << favouriteNumber << std::endl;
+
+    return 0;
+}
