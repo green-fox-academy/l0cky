f754ad0e4bd2d2da37621b1b97b6b6ae5cc1727a
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 12:58:19 +0200

    Complete HelloOthers exercise.

---
+++ week-01/day-3/HelloOthers/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+    std::cout << "Hello Viktor!" << std::endl;
+    std::cout << "Hello Gyozo!" << std::endl;
+    std::cout << "Hello Donat!" << std::endl;
+
+    return 0;
+}
