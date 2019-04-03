36e1531e08f9a4b2934f67aff9a58cccd7ef7e7b
breki <berki.csaba.jozsef@gmail.com>
2019-04-03 12:52:21 +0200

    Complete HumptyDumpty exercise.

---
+++ week-01/day-3/HumptyDumpty/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+
+    // Modify this program to print Humpty Dumpty riddle correctly
+    std::cout << "Humpty Dumpty sat on a wall," << std::endl;
+    std::cout << "Humpty Dumpty had a great fall." << std::endl;
+    std::cout << "All the king's horses and all the king's men" << std::endl;
+    std::cout << "Couldn't put Humpty together again." << std::endl;
+
+    return 0;
+}
