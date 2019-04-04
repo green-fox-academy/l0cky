fbae644e45b6a1154a67d657726639b538d0f1df
breki <berki.csaba.jozsef@gmail.com>
2019-04-04 13:28:43 +0200

    Complete Third exercise.

---
+++ week-01/day-4/Third/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `q`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print the third element of `q`
+
+    int q[] = {4, 5, 6, 7};
+
+    std::cout << q[2] << std::endl;
+
+    return 0;
+}
