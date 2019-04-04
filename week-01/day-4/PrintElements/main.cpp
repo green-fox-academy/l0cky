fdb545f3fa6e616f24f4a4dfce0c7e3acea9d3f9
breki <berki.csaba.jozsef@gmail.com>
2019-04-04 13:59:15 +0200

    Complete PrintElements exercise.

---
+++ week-01/day-4/PrintElements/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `numbers`
+    //   with the following content: `[4, 5, 6, 7]`
+    // - Print all the elements of `numbers`
+
+    int numbers[] = {4, 5, 6, 7};
+
+    for (int i = 0; i < sizeof(numbers) / sizeof(numbers[0]); i++) {
+        std::cout << numbers[i] << std::endl;
+    }
+
+    return 0;
+}
