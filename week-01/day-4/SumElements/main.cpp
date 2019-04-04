b7ec87d724aad1b136efe0a1a7abc4b09c11b6d9
breki <berki.csaba.jozsef@gmail.com>
2019-04-04 13:39:01 +0200

    Complete SumElements exercise.

---
+++ week-01/day-4/SumElements/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `r`
+    //   with the following content: `[54, 23, 66, 12]`
+    // - Print the sum of the second and the third element
+
+    int r[] = {54, 23, 66, 12};
+
+    std::cout << r[1] + r[2] << std::endl;
+
+    return 0;
+}
