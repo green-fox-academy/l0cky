ba1dc2f7a1484322a7ea26c4c70346d5b687810d
breki <berki.csaba.jozsef@gmail.com>
2019-04-08 15:12:45 +0200

    Refactor and use functions.

--- week-02/day-1/PrintAddresses/main.cpp
+++ week-02/day-1/PrintAddresses/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+void inputNumbers(int *arr, int arrSize);
+void printMemAddress(int *arr, int arrSize);
+
 int main()
 {
     // Create a program which accepts five integers from the console (given by the user)
@@ -7,18 +10,25 @@ int main()
     // print out the memory addresses of the elements in the array
 
     int arr[5] = {};
-    int * arrPointer = arr;
+    int arrSize = sizeof(arr) / sizeof(int);
 
-    std::cout << "This program print the memory address of the 5 intiger." << std::endl;
+    std::cout << "This program print the memory address of the 5 given intiger." << std::endl;
 
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
+    inputNumbers(arr, arrSize);
+    printMemAddress(arr, arrSize);
+
+    return 0;
+}
+void inputNumbers(int *arr, int arrSize)
+{
+    for (int i = 0; i < arrSize; i++) {
         std::cout << i + 1 << ". number: ";
         std::cin >> arr[i];
     }
-
-    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
-        std::cout << i + 1 << ". number's memory address is: " << arrPointer + i << std::endl;
+}
+void printMemAddress(int *arr, int arrSize)
+{
+    for (int i = 0; i < arrSize; i++) {
+        std::cout << i + 1 << ". number's memory address is: " << arr + i << std::endl;
     }
-
-    return 0;
 }
