600822188c696868cedf649ae8570ee523bdf290
breki <berki.csaba.jozsef@gmail.com>
2019-04-08 13:27:27 +0200

    Complete FiveNumbers exercise.

---
+++ week-02/day-1/FiveNumbers/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+    // Create a program which accepts five integers from the console (given by the user)
+    // and store them in an array
+    // print out the values of that array using pointers again
+
+    int arr[5] = {};
+    int * arrPointer = arr;
+
+    std::cout << "This program print the memory address of the 5 intiger." << std::endl;
+
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
+        std::cout << i + 1 << ". number: ";
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < sizeof(arr) / sizeof(int); i++) {
+        std::cout << i + 1 << ". number's memory address is: " << *(arrPointer + i) << std::endl;
+    }
+
+    return 0;
+}
