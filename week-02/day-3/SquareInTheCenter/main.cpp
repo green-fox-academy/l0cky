12d107e75cb23387eea11aea793271c9503d32f1
breki <berki.csaba.jozsef@gmail.com>
2019-04-10 16:05:37 +0200

    Complete Square in the center exercise.

---
+++ week-02/day-3/SquareInTheCenter/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <SDL.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//Draws geometry on the canvas
+void draw();
+
+// Draw a square
+void square(std::pair<int, int> cord, int width);
+
+//Starts up SDL and creates window
+bool init();
+
+//Frees media and shuts down SDL
+void close();
+
+//The window we'll be rendering to
+SDL_Window* gWindow = nullptr;
+
+//The window renderer
+SDL_Renderer* gRenderer = nullptr;
+
+void draw()
+{
+    // Draw a green 100x100 square to the canvas' center.
+    int witdh = 100;
+    std::pair<int, int> center{SCREEN_WIDTH/2, SCREEN_HEIGHT/2};
+    std::pair<int, int> startCord{center.first - witdh / 2, center.second - witdh / 2};
+
+    SDL_SetRenderDrawColor( gRenderer, 0x00, 0xFF, 0x00, 0xFF );
+    square(startCord, witdh);
+
+}
+
+void square(std::pair<int, int> cord, int width)
+{
+    SDL_Rect rectangle;
+    rectangle.x = cord.first;
+    rectangle.y = cord.second;
+    rectangle.w = width;
+    rectangle.h = width;
+    SDL_RenderDrawRect( gRenderer, &rectangle);
+
+}
+
+bool init()
+{
+    //Initialize SDL
+    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+    {
+        std::cout << "SDL could not initialize! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Create window
+    gWindow = SDL_CreateWindow( "Centered square", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( gWindow == nullptr )
+    {
+        std::cout << "Window could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Create renderer for window
+    gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+    if( gRenderer == nullptr )
+    {
+        std::cout << "Renderer could not be created! SDL Error: " << SDL_GetError() << std::endl;
+        return false;
+    }
+
+    //Initialize renderer color
+    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+    return true;
+}
+
+void close()
+{
+    //Destroy window
+    SDL_DestroyRenderer( gRenderer );
+    SDL_DestroyWindow( gWindow );
+    gWindow = nullptr;
+    gRenderer = nullptr;
+
+    SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+    //Start up SDL and create window
+    if( !init() )
+    {
+        std::cout << "Failed to initialize!" << std::endl;
+        close();
+        return -1;
+    }
+
+    //Main loop flag
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+
+    //While application is running
+    while( !quit ) {
+        //Handle events on queue
+        while (SDL_PollEvent(&e) != 0) {
+            //User requests quit
+            if (e.type == SDL_QUIT) {
+                quit = true;
+            }
+        }
+
+        //Clear screen
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(gRenderer);
+
+        draw();
+
+        //Update screen
+        SDL_RenderPresent(gRenderer);
+    }
+
+    //Free resources and close SDL
+    close();
+
+    return 0;
+}
