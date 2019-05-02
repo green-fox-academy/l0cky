fb6126b42847262b119dc15ae2eac2fc8c607fa2
breki <berki.csaba.jozsef@gmail.com>
2019-05-02 10:43:59 +0200

    feat(exercise): complete Reversed lines

---
+++ week-03/day-2/ReversedLines/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+
+void reverseLine(std::string line);
+
+int main()
+{
+    std::ifstream myFile;
+    myFile.exceptions(std::ifstream::badbit);
+
+    try {
+        myFile.open("../reversed-lines.txt");
+
+        std::string line;
+
+        while (std::getline(myFile, line)) {
+            reverseLine(line);
+        }
+
+    } catch (std::ifstream::failure& e) {
+        std::cout << e.what() << std::endl;
+    }
+
+
+    return 0;
+}
+
+void reverseLine(std::string line)
+{
+    for (std::string::reverse_iterator it = line.rbegin(); it != line.rend(); ++it) {
+        std::cout << *it;
+    }
+    std::cout << std::endl;
+}
