73ef1604bcbbb05d5047703de22e9a940721f412
breki <berki.csaba.jozsef@gmail.com>
2019-05-14 13:27:54 +0200

    feat(week-06/day-2): complete introduce yourself

---
+++ week-06/day-2/introduceYourself/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Use variables to store the informations!
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+    printf("%s\n", "l0cky");
+    printf("%d\n", 35);
+    printf("%.2f\n", 1.75);
+
+    return 0;
+}
