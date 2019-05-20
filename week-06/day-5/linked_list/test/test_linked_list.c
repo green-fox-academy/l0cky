4d7d74c87c2cfb8b28b26d391b96c1a971574182
breki <berki.csaba.jozsef@gmail.com>
2019-05-20 16:39:43 +0200

    fix(w06/d5): add free() to tests

--- week-06/day-5/linked_list/test/test_linked_list.c
+++ week-06/day-5/linked_list/test/test_linked_list.c
@@ -1,27 +1,30 @@
+#include <stdlib.h>
 #include "../vendor/ceedling/vendor/unity/src/unity.h"
 #include "../src/linked_list.h"
 
+linked_list_t *head_ptr = NULL;
+
 void setUp(void)
 {
+    head_ptr = create_linked_list();
 }
 
 void tearDown(void)
 {
+    free(head_ptr);
+    head_ptr = NULL;
 }
 
 void test_create_linked_list_data(void)
 {
     int data = 0;
-    linked_list_t *head_ptr = create_linked_list();
-
     TEST_ASSERT_EQUAL_INT(0, head_ptr->data);
+
 }
 
 void test_create_linked_list_next(void)
 {
-    int *data = NULL;
-    linked_list_t *head_ptr = create_linked_list();
-
+    linked_list_t *data = NULL;
     TEST_ASSERT_EQUAL_INT(data, head_ptr->next);
 }
 
