c987b406beed32214590fee6d8596fee2fb3028a
breki <berki.csaba.jozsef@gmail.com>
2019-05-21 15:48:34 +0200

    feat(week07/d2): complete Flashing LED

---
+++ week-07/day-2/FlashingLED/main.c
@@ -0,0 +1,36 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+			
+
+int main(void)
+{
+    HAL_Init();
+
+    BSP_PB_Init(BUTTON_KEY, BUTTON_MODE_GPIO);
+
+
+    BSP_LED_Init(LED1);
+
+    while (1) {
+        /* If button is pressed do something */
+        if (BSP_PB_GetState(BUTTON_KEY) == 1) {
+        	while (BSP_PB_GetState(BUTTON_KEY) == 1) {
+        		BSP_LED_On(LED1);
+        		HAL_Delay(250);
+        		BSP_LED_Off(LED1);
+        		HAL_Delay(250);
+        	}
+        }
+    }
+}
